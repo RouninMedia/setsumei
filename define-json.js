2d2d43b13292c056525cd038904ae52066dc2e33
Alan Lansdowne <alan_lansdowne@yahoo.com>
2021-12-17 18:43:00 +0100

    Create define-json.js

---
+++ define-json.js
@@ -0,0 +1,26 @@
+/**
+ * JSON Data.
+ * @typedef {string} JSON
+ */
+
+
+// FUNCTION SIGNATURE
+
+// Template:
+
+/**
+ * [ Function Description ]
+ * @function [ Function Name ]
+ * @param {[ Parameter Type ]} [ Parameter Name ] - [ Parameter Description ]
+ * @returns {[ Return Type ]} [ Return Description ]
+ */
+
+
+// Example:
+
+/**
+ * Escapes JSON data to the QuasiJSON format.
+ * @function convertJSONToQuasiJSON
+ * @param {JSON} realJSON - any JSON data
+ * @returns {string} the QuasiJSON escape corresponding to the JSON data input
+ */
