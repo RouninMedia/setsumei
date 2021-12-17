5c069e16d796d1a3c58ecff338130ca99cc28748
Alan Lansdowne <alan_lansdowne@yahoo.com>
2021-12-17 18:40:11 +0100

    Create function-signature.js

---
+++ function-signature.js
@@ -0,0 +1,19 @@
+# Function Signature
+
+## Template:
+
+/**
+ * [ Function Description ]
+ * @function [ Function Name ]
+ * @param {[ Parameter Type ]} [ Parameter Name ] - [ Parameter Description ]
+ * @returns {[ Return Type ]} [ Return Description ]
+ */
+
+## Example:
+
+/**
+ * Escapes JSON data to the QuasiJSON format.
+ * @function convertJSONToQuasiJSON
+ * @param {JSON} realJSON - any JSON data
+ * @returns {string} the QuasiJSON escape corresponding to the JSON data input
+ */
